6af6dc004b3c21f43c39239c88b0893f39b4ac13
Rex Feng <rexfeng@gmail.com>
2016-03-01 14:43:28 -0800

    update Stack simple balanced parens exercise to include non-paren symbols

--- basic_data_structures/exercises/simple_balanced_parentheses.rb
+++ basic_data_structures/exercises/simple_balanced_parentheses.rb
@@ -13,20 +13,30 @@ class BalancedCheck
   def balanced?
     stack = Stack.new
     balance = true
+    valid_balance_openers = ["(", "[", "{"]
 
     input_string.split("").each_with_index do |character, index|
-      if character == "("
+      if valid_balance_openers.include? character
         stack.push(character)
       else
         if stack.is_empty?
           balance = false
         else
-          stack.pop
+          popped = stack.pop
+          balance = false if !symbols_match(popped, character)
         end
       end
     end
 
     balance && stack.is_empty?
+  end
+
+  private
+
+  def symbols_match(open_symbol, close_symbol)
+    opens = "([{"
+    closes = ")]}"
+    opens.index(open_symbol) == closes.index(close_symbol)
   end
 
 end
@@ -48,3 +58,25 @@ puts "Checking balanced_1, 2, 3..."
 puts BalancedCheck.new(balanced_1).balanced?
 puts BalancedCheck.new(balanced_2).balanced?
 puts BalancedCheck.new(balanced_3).balanced?
+
+puts "Going above and beyond, check with other symbols: [] {}..."
+
+puts "Checking not_balanced_4, 5, 6..."
+
+not_balanced_4 = "[[((()]))"
+not_balanced_5 = "[{()]"
+not_balanced_6 = "([)]"
+
+puts BalancedCheck.new(not_balanced_4).balanced?
+puts BalancedCheck.new(not_balanced_5).balanced?
+puts BalancedCheck.new(not_balanced_6).balanced?
+
+puts "Checking balanced_4, 5, 6..."
+
+balanced_4 = "[[{{(())}}]]"
+balanced_5 = "{{([][])}()}"
+balanced_6 = "[][][](){}"
+
+puts BalancedCheck.new(balanced_4).balanced?
+puts BalancedCheck.new(balanced_5).balanced?
+puts BalancedCheck.new(balanced_6).balanced?
