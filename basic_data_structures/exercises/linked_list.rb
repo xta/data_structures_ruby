b574c99a0cab56dd7ede8042ba42710be7eb29b9
Rex Feng <rexfeng@gmail.com>
2016-03-08 16:12:40 -0800

    create linked_list exercise file

---
+++ basic_data_structures/exercises/linked_list.rb
@@ -0,0 +1,17 @@
+require_relative '../list.rb'
+
+# Exercise: work with linked list(s)
+
+# Notes
+# * linked lists allow us to maintain a collection of relative items. there is no requirement that they are in contiguous memory
+# * the location of the first item (head) must be tracked
+# * each item should know the location of the next item
+# * the last item should know that there is no next item
+
+class Node
+  # todo
+end
+
+class UnorderedList
+  # todo
+end
